7d1a96673502a03eda78a5f44834f127a53661c6
root <root@debian.fritz.box>
2013-04-11 15:26:16 +0100

    add secret key to config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 py={}
 
-py['sitename'] = "neunr"
+py['sitename'] = "pgpy"
 py['mediadir'] = 'static/pics/'
 py['webres'] =  1024
 py['thumbres'] = 300
 py['supported_file'] = ['jpg', 'png', 'bmp']
-
+py['secret'] = '\x18\xcax\xcb\x0c\xeb\x91|\xd8d\xba\x0f\x0f\x14\xf2C}SC\x97\xc7|U\x16'
 
